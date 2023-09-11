626771c5051bf21ed2f28db986f68501b041a11f
davidnjau <davidnjau21@gmail.com>
2023-09-11 12:20:30 +0300

    first commit

---
+++ src/main/java/com/easymeal/easymealgdc/EasymealGdcApplication.java
@@ -0,0 +1,13 @@
+package com.easymeal.easymealgdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymealGdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymealGdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easymeal/easymealgdc/EasymealGdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymeal.easymealgdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymealGdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
